8e5ed11f5336c023b85ea90a3d24d2011953a29d
phil <phil@phil-Satellite-L350.(none)>
2016-04-05 11:21:05 +0200

    install ripl

---
+++ install_riplbox.sh
@@ -0,0 +1,31 @@
+RipL-POX (Ripcord-Lite for POX): A simple network controller for OpenFlow-based data centers
+
+== Install ==
+RipL, POX, and setuptools are the dependencies.
+
+This version has been tested with the master branch of POX, commit 0a1bbb8, made on Sat Nov 24 01:52:07 2012 -0800.
+
+The invocation examples assume that POX available in ~/.
+
+# Add POX at tested version:
+cd ~/
+git clone git://github.com/noxrepo/pox
+cd pox
+git checkout 0a1bbb8
+
+# Build RipL:
+cd ~/
+sudo apt-get install -y python-setuptools
+git clone git://github.com/brandonheller/ripl.git
+cd ripl
+sudo python setup.py develop
+
+# Building Ripcord-POX
+cd ~/
+git clone git://github.com/brandonheller/riplpox.git
+cd riplpox
+sudo python setup.py develop
+
+== Running on POX ==
+
+See README
